68bd0f610b41235cd0a80cbc2c84125f2eaec5f8
Nick Zinck <30390163+nzinck17@users.noreply.github.com>
2018-01-31 13:46:13 -0500

    Update and rename Modules2/Outputs/plot_time_depth_wq.R to modules2/outputs/plot_time_depth_wq.R

--- modules2/outputs/plot_time_depth_wq.R
+++ modules2/outputs/plot_time_depth_wq.R
@@ -1,5 +1,5 @@
 ##############################################################################################################################
-#     Title: Plot-Time-Depth.R
+#     Title: plot_time_depth_wq.R
 #     Type: Secondary Module for DCR Shiny App
 #     Description: Time Series plot (for non-depth dependent data)
 #     Written by: Nick Zinck, Spring 2017
